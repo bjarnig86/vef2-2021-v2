7e979534209ac6d7f0a2813bb3ac66d1341bb7a4
bjarnig86 <72604468+bjarnig86@users.noreply.github.com>
2021-02-03 17:06:13 +0000

    bla

--- src/app.js
+++ src/app.js
@@ -3,13 +3,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-app.use(express.urlencoded({ extended: true }));
-
 const { PORT: port = 3000 } = process.env;
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+
 // TODO setja upp rest af virkni!
+
 app.get('/', (req, res) => {
   res.send(`
 <form method="post" action="/post" enctype="application/x-www-form-urlencoded">
